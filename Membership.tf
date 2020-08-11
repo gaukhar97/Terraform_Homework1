2f913acaa5146b1d9a678314eec7e13aea776f89
“gaukhar97” <imankulova@yahoo.com>
2020-08-11 16:13:26 +0000

    some files

--- Membership.tf
+++ Membership.tf
@@ -2,27 +2,7 @@ resource "aws_iam_group_membership" "Developers_team" {
   name = "Developers-group-membership"
 
   users = [
-    "${aws_iam_user.Bob.name}",
-  ]
-
-  group = "${aws_iam_group.Developer.name}"
-}
-
-resource "aws_iam_group_membership" "Developers_team" {
-  name = "Developers-group-membership"
-
-  users = [
-    "${aws_iam_user.Tim.name}",
-  ]
-
-  group = "${aws_iam_group.Developer.name}"
-}
-
-resource "aws_iam_group_membership" "Developers_team" {
-  name = "Developers-group-membership"
-
-  users = [
-    "${aws_iam_user.Ben.name}",
+    "${aws_iam_user.Bob.name}", "${aws_iam_user.Tim.name}", "${aws_iam_user.Ben.name}",
   ]
 
   group = "${aws_iam_group.Developer.name}"
